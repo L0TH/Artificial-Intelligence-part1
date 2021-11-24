2585994cae638e24cf266c527dfb1dec5ef315d4
konstantinosKatsamis <75335809+konstantinosKatsamis@users.noreply.github.com>
2021-11-24 23:33:54 +0200

    Chromosome class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-    public static void main(String[] args) {
-        System.out.println("hello main");
+        // GeneticAlgorithm algorithm = new GeneticAlgorithm();
+        Chromosome solution = new Chromosome(10);
+            //populationSize, mutationProbability, maximumSteps, minimumFitness
+        //algorithm.run(1000, 0.08, 1000, 28); // to 28 tha allaksei analoga me to (n-1) + (n-2) + (n-3) ... + (n-n)
+                                                                    // alios na ginete aftomata
+        solution.print();
+
+        // kodikas apo front
+        /*
+        GeneticAlgorithm algorithm = new GeneticAlgorithm();
+        //populationSize, mutationProbability, maximumSteps, minimumFitness
+        Chromosome solution = algorithm.run(1000, 0.08, 1000, 28);
+        solution.print();
+        */
     }
-
 }
