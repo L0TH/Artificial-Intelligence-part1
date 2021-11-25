702e93ccb8727161850bcc6254d4e99327edd95c
Ioannis Apostolou <giannistolou@protonmail.com>
2021-11-25 16:04:57 +0200

    copy paste lab code

--- src/com/aueb/QueenChess/Main.java
+++ src/com/aueb/QueenChess/Main.java
@@ -1,8 +1,12 @@
 package com.aueb.QueenChess;
 
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("hello queen");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        GeneticAlgorithm algorithm = new GeneticAlgorithm();
+        //populationSize, mutationProbability, maximumSteps, minimumFitness
+        Chromosome solution = algorithm.run(1000, 0.08, 1000, 28);
+        solution.print();
     }
 }
